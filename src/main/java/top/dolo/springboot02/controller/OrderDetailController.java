4066c8db9e421630d23c4a247697ac14000929e0
dololty <31910845+DoLolitingyu@users.noreply.github.com>
2018-07-12 21:32:10 +0800

    6. 删除无效类

--- src/main/java/top/dolo/springboot02/controller/OrderDetailController.java
+++ src/main/java/top/dolo/springboot02/controller/OrderDetailController.java
@@ -16,14 +16,5 @@ public class OrderDetailController {
     @Autowired
     OrderDetailServiceImpl orderDetailService;
 
-//    //获取某个订单详情
-//    @GetMapping("/oriderdetal/{id}")
-//    public String findAll(@PathVariable("id") Integer id, Model model, HttpSession session){
-//        String userName = (String)session.getAttribute("LoginName");
-//        User user = userDAO.findByName(userName);
-//        List<Order> orders = orderService.findAllByUserId(user.getId());
-//        model.addAttribute("orders", orders);
-//        return "order/list";
-//    }
 
 }
